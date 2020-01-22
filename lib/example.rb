fa31fbf5077775ae19cae2ab9aec9144f6fc197e
David Van Fleet <dvfleet413@gmail.com>
2020-01-22 00:45:25 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,17 +1,7 @@
 # Use this space to code along with the readme
 
-def say_hello_world_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
+def greeting
+  puts "Hello World!"
 end
 
-say_hello_world_ten_times
+greeting
